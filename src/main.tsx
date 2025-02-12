76ff41ab00f9760d0e961218193f21a4dd17522b
hzkm613 <hzkm613@gmail.com>
2025-02-12 13:25:16 -0500

    Set up React Query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from 'styled-components';
 import { theme } from "./theme.ts";
+import { QueryClient ,QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <ThemeProvider theme={theme}>
     <App />
     </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
